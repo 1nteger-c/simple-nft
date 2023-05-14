cfd3690aa36bdd2a4918258c69e6ee9be3e8ee77
1nteger-c <korea.1nteger@gmail.com>
2023-05-14 21:55:07 +0900

    feat: add code to call contract's mintNFT function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,51 +3,44 @@ import ImageUploader from "./components/ImageUploader";
 import './App.css'
 import { useState, useEffect } from 'react'
 import detectEthereumProvider from '@metamask/detect-provider'
+import { ethers } from 'ethers';
 
 const App = () => {
-  const [hasProvider, setHasProvider] = useState<boolean | null>(null)
-  const initialState = { accounts: [] }
-  const [wallet, setWallet] = useState(initialState)
+  const [signer, setSigner] = useState<ethers.Signer | null>(null);
+  const [wallet, setWallet] = useState<string | null>(null);
 
-  useEffect(() => {
-    const refreshAccounts = (accounts: any) => {                /* New */
-      if (accounts.length > 0) {                                /* New */
-        updateWallet(accounts)                                  /* New */
-      } else {                                                  /* New */
-        // if length 0, user is disconnected                    /* New */
-        setWallet(initialState)                                 /* New */
-      }                                                         /* New */
-    }                                                           /* New */
-
-    const getProvider = async () => {
-      const provider = await detectEthereumProvider({ silent: true })
-      setHasProvider(Boolean(provider))
-
-      if (provider) {                                           /* New */
-        const accounts = await window.ethereum.request(         /* New */
-          { method: 'eth_accounts' }                            /* New */
-        )                                                       /* New */
-        refreshAccounts(accounts)                               /* New */
-        window.ethereum.on('accountsChanged', refreshAccounts)  /* New */
-      }                                                         /* New */
-    }
-
-    getProvider()
-    return () => {                                              /* New */
-      window.ethereum?.removeListener('accountsChanged', refreshAccounts)
-    }                                                           /* New */
-  }, [])
-
-  const updateWallet = async (accounts:any) => {
-    setWallet({ accounts })
-  }
 
   const handleConnect = async () => {
-    let accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    })
-    updateWallet(accounts)
-  }  
+    try {
+      // detect if Metamask is installed and available
+      const provider = await detectEthereumProvider();
+
+      if (!provider) {
+        console.error('Metamask not found');
+        return;
+      }
+
+      // request permission to access user's accounts
+      if ((provider as any).request && (provider as any).request({ method: 'eth_accounts' })) {
+        await (provider as any).request({ method: 'eth_requestAccounts' });
+      } else {
+        // if the provider doesn't have a request method, fall back to the legacy API
+        await (provider as any).enable();
+      }
+
+      // get the signer object for the currently connected account in Metamask
+      const ethersProvider = new ethers.providers.Web3Provider(provider);
+      const signer = ethersProvider.getSigner();
+      setSigner(signer);
+
+      // get the current account address from Metamask
+      const accounts = await ethersProvider.listAccounts();
+      setWallet(accounts[0]); // assuming there is at least one account connected
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
 
   return (
     <div style={{ textAlign: "center" }}>
@@ -56,15 +49,15 @@ const App = () => {
       >
         NFT Generator
       </header>
-      <ImageUploader />
-      <div> {hasProvider} Exist</div>
+      <ImageUploader signer={signer} wallet={wallet} />
+      {wallet ? (
+          <p>Wallet address: {wallet}</p>
+        ) : (
+          window.ethereum?.isMetaMask && (
+            <button onClick={handleConnect}>Connect MetaMask</button>
+          )
+        )}
       
-      { window.ethereum?.isMetaMask && wallet.accounts.length < 1 &&  /* Updated */
-        <button onClick={handleConnect}>Connect MetaMask</button>
-      }
-      { wallet.accounts.length > 0 &&                /* New */
-        <div>Wallet Accounts: { wallet.accounts[0] }</div>
-      }
     </div>
   );
 }
--- frontend/src/components/ImageUploader.tsx
+++ frontend/src/components/ImageUploader.tsx
@@ -1,8 +1,16 @@
 import React, { useState, useRef, ChangeEvent, FormEvent } from "react";
+import { ethers } from 'ethers';
 
-function ImageUploader() {
-  console.log("QWE")
+type ImageUploaderProps = {
+  signer: ethers.Signer | null;
+  wallet: string | null;
+};
+
+
+function ImageUploader({ signer, wallet }: ImageUploaderProps) {
   const [imageUrl, setImageUrl] = useState("");
+  const [imageDBUrl, setImageDBUrl] = useState<String | null>(null);
+
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleFileInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -33,14 +41,57 @@ function ImageUploader() {
       }
 
       const result = await response.json();
+      setImageDBUrl("http://127.0.0.1:8080/img/" + result.imageUrl)
       return result.imageUrl;
     }
   };
-  const handleMintClick = () => {
-    // Logic for minting the image goes here
-    console.log("minting the image");
-  };
+  const handleMintClick = async () => {
+    if (imageDBUrl) {
+      console.log("Image URL:", imageDBUrl);
+      const address = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e";
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const abi = [
+        "function mintNFT(address user, string memory tokenURI) public returns (uint256)",
+        "function getTokenIds() public view returns (uint256)",
+        "event Mint(address user, uint256 tokenId)"
+      ];
+      if (!signer) {
+        throw new Error("Signer is null.");
+      }
+      const contract = new ethers.Contract(address, abi, signer);
   
+      try {
+        const tx = await contract.mintNFT(wallet, imageDBUrl);
+        console.log(`Transaction hash: ${tx.hash}`);
+      
+        const receipt = await provider.waitForTransaction(tx.hash);
+        console.log(`Transaction confirmed in block ${receipt.blockNumber}`);
+    
+        setTimeout(async () => {
+          const events = await contract.queryFilter(contract.filters.Mint(), receipt.blockNumber, receipt.blockNumber);
+          if (events.length > 0) {
+            const event = events[0];
+            if (event.args && event.args.tokenId) {
+              console.log(`New NFT minted with tokenId ${event.args.tokenId.toString()}`);
+              alert(`The transaction succeeded - tokenId : ${event.args.tokenId.toString()}`);
+            } else {
+              console.log("Event found but tokenId not present");
+            }
+          } else {
+            console.log("Failed to find Mint event in receipt");
+          }
+        
+        }, 5000); // wait 5 seconds before checking for event
+      } catch (error) {
+        console.error("Error minting NFT:", error);
+      }    
+    
+    } else {
+      alert("Upload image first")
+      console.log("Upload img first");
+    }
+  };
+    
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
